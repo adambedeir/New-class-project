c11d1b628ca024549cbf9341bfd17e0bccc09f1b
adambedeir <adambedeir@gmail.com>
2020-07-09 19:34:25 +0000

    does it Realy matter what I type here?

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
 import random
-rand=random.randint(0,10)
-tries=5
-Win = False
-Play = True
-while Play == True :
-  if tries>0:
-    guess=input('pick a number between 0 and 10 \n')
-    guess=int(guess)
-    
-    if (guess<0 or guess > 10) :
-      print('I am afraid you just lost a turn ')
-      tries=tries-1
-      break
-    else:
-      if guess>rand:
-        bigger = guess - rand
-        print('you are'+ ' '+ str(bigger) + ' ' +'off the correct answer')
-        tries=tries-1
-      elif guess<rand:
-        smaller= rand- guess
-        print('you are'+ ' ' + str(smaller) + ' ' +'off the correct answer')
-        tries=tries-1
-      else:
-        print( 'you got the number right')
-        answer= input('do you wnat to play again? Y/N')
-        if answer== 'Y':
-  else tries:
-    print( 'no more turns')
-    print( 'the number was' + ' ' +str(rand))
-    Play=False
+print('Tictactoe')
+def game() :
+  answer = input('1 player or 2 (type 1 or 2)\n')
+  if answer == str(1):
+    possiblespaces=['top left','top middle','top right','middle right', 'center', 'middle left','bottom']
+    print('it works')
+  elif answer == str(2):
+    print('hello')
+  else:
+    print('pick one or two please')
+    game()
+game()
